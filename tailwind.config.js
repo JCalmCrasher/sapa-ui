10b861cd7385519232b5753aff1955b775e5659f
JoshX <joshx.dev@gmail.com>
2023-12-05 14:19:12 +0100

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx}"],
  theme: {
   extend: {
-    colors: {
-        'text-dark': '#001B1F',
-        'text-darker': '#474747',
-        'green-primary': '#00545F',
-    
-      },
-      fontFamily: {
-        'serif': ['"Space Grotesk"']
-      },
+   colors: {
+    dark: "#001B1F",
+    darker: "#474747",
+    secondary: "#00545F",
+    primary: "#EBFFB7"
+   },
+   fontFamily: {
+    serif: ['"Space Grotesk"']
+   }
   }
  },
  plugins: []
