f1abd05fd76742b29c22fed9d864b4369f36a90a
JoshX <joshx.dev@gmail.com>
2023-12-05 14:51:53 +0100

    config: change `container` class preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx}"],
  theme: {
+  container: {
+    center: true,
+    screens: {
+     "3xl": "2560px"
+    }
+   },
+   screens: {
+    xs: "320px",
+    sm: "640px",
+    md: "920px",
+    lg: "1024px",
+    xl: "1440px",
+    "2xl": "1680px",
+    "3xl": "2560px"
+   },
   extend: {
    colors: {
     dark: "#001B1F",
